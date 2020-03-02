b2124134f3219f433ee0863bacf5307f406d9543
jeysona <jeysonac@gmail.com>
2020-03-02 14:05:46 -0500

    Ejericio de regresión lineal múltiple en R finalizado

--- Exercises/Regression/multiple_linear_regression.r
+++ Exercises/Regression/multiple_linear_regression.r
@@ -26,5 +26,36 @@ regression = lm(formula = Profit ~ .,
 # Predicción de los resultados del conjunto de prueba
 y_pred = predict(regression,newdata = testing_set)
 
+#Modelo óptimo con eliminación hacia atrás
+# regression = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State, 
+#                 data = dataset)
+# 
+# regression = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend, 
+#                 data = dataset)
+# 
+# regression = lm(formula = Profit ~ R.D.Spend + Marketing.Spend, 
+#                 data = dataset)
+# 
+# regression = lm(formula = Profit ~ R.D.Spend, 
+#                 data = dataset)
+# 
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
+
 
 summary(regression)
